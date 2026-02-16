df726542a3b70f9e26dd1f69461c0cb0435c6301
David Kamau <davidkamau.mec@gmail.com>
2026-02-16 15:40:48 +0300

    Create: Handle sign up function

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ class userConstructor{
     this.username = username;
     this.password = password;
   }
+};
+
+function handleSignUp(){
+  const nameInput = document.getElementById('username')
+  const passwordInput = document.getElementById('password')
+  const passwordConfirmInput = document.getElementById('confirmPassword')
+
+  const nameVal = nameInput.value;
+  const passwordValue = passwordInput.value;
+  const passwordConfirmValue = passwordConfirmInput.value;
+
 }
 
 const newUser = new userConstructor("David", "David1234")
